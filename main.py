84b1e71f2a10e955cc747aa98a26bb866e6bf002
henriquecdb <henriquecamposj@gmail.com>
2022-09-19 16:13:10 -0300

    Pilot Test

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from metodos import *
 
 
 if __name__ == "__main__":
-    print(falsa_posicao(1.5, 2, 10**-4, 3))
-    print(newton(-5, 0.0001, 4))
+    print(falsa_posicao(2, 3, 0.004, 7))
+    print(newton(3, 0.004, 7))
--- metodos.py
+++ metodos.py
@@ -2,34 +2,33 @@ from pylab import *
 
 
 def f(x):
-    return (x/2)**2 - sin(x)
+    return x**4 - 78
 
 
 def df(x):
-    return x/2 - cos(x)
+    return 4*x**3
 
 
-def falsa_posicao(a, b, e, itmax):
-    it = 0
+def falsa_posicao(a, b, tol, kmax):
+    k = 0
     x = a
     er = 1
-    while er >= e and it < itmax:
-        xold = x
-        x = a - f(a)*(b-a)/(f(b)-f(a))
-        er = abs((x-xold)/x)
+    while er >= tol and k < kmax:
+        xo = x
+        x = (a*f(b) - b*f(a))/(f(b)-f(a))
+        er = abs((x-xo)/x)
         if f(a)*f(x) < 0:
             b = x
         else:
             a = x
-        it += 1
-    return x, er, it
+        k += 1
+    return x, er, k
 
 
-def newton(xo, e, itmax):
-    it = 1
-    while abs(f(xo)) > e and it < itmax:
+def newton(xo, tol, itmax):
+    it = 0
+    while abs(f(xo)) > tol and it < itmax:
         x = xo - f(xo)/df(xo)
         xo = x
         it += 1
-    return xo, it, f(xo)
-
+    return xo, f(xo), it
